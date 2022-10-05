2038a4d520e7fad0857bb8551ad514d01c81c284
LeandroGomez1 <leandrodanielgomez@gmail.com>
2022-10-05 16:08:24 -0300

    Practica 2 finalizada

--- practica-mips/practica2/ej1.asm
+++ practica-mips/practica2/ej1.asm
@@ -3,11 +3,11 @@
 	res: .space 3		#Espacio de 3 bytes para el resultado
 	
 .text
-	lb $t0, V			#Carga cada valor del vector V en los registros
-	lb $t1, V + 1
-	lb $t2, V + 2
-	lb $t3, V + 3
-	lb $t4, V + 4
+	lb $t0, V			#Carga V[1] en $t0
+	lb $t1, V + 1		#Carga V[2] en $t1
+	lb $t2, V + 2		#Carga V[3] en $t2
+	lb $t3, V + 3		#Carga V[4] en $t3
+	lb $t4, V + 4		#Carga V[5] en $t4
 	and $t5, $t0, $t4	# (V[1] and V[5]) 			-> $t5
 	or $t6, $t1, $t3	# (V[2] or V[4]) 			-> $t6
 	or $t7, $t0, $t1	# (V[1] or V[2]) 			-> $t7
